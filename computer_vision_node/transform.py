35bac54f46663facc136ea0fc753b9dc2e4578a9
Atif Mahmud <atifmahmud@uwaterloo.ca>
2018-11-21 18:25:01 -0500

    Implement getPerspectiveTransform method
    References: #12
    
    Signed-off-by: Atif Mahmud <atifmahmud@uwaterloo.ca>

--- computer_vision_node/transform.py
+++ computer_vision_node/transform.py
@@ -37,3 +37,29 @@ class PerspectiveTransform:
 
 	def __str__(self):
 		return str(self.matrix)
+
+def getPerspectiveTransform(src_img, src_points, output_shape):
+	"""Gets the transform matrix that will map the 4 points `src_points` to the four corners of a flat plane of shape `output_shape`.
+	
+	Args:
+	    src_img (numpy.ndarray): Image that `src_points` are from.
+	    src_points (numpy.ndarray): Four points describing the four corners of the output plane (`shape=(4,2)`).
+	    output_shape (tuple): `(height, width)` of the output plane.
+	
+	Returns:
+	    PerspectiveTransform: `PerspectiveTransform` object encapsulating the resulting transform matrix.
+	"""
+	output_shape = (output_shape[1], output_shape[0])
+	
+	# Determine which input points correspond to which of the 4 corners
+	img_corners = _fourCorners(src_img.shape[:2])
+	dists = np.array([np.linalg.norm(img_corner - src_points, axis=1) for img_corner in img_corners])
+	correspondences = np.argmin(dists, axis=1) # img_corner i corresponds to the src_point at correspondences[i]
+	assert np.unique(correspondences).shape[0] == 4
+
+	src = np.array([src_points[i] for i in correspondences], dtype=np.float32)
+	dst = _fourCorners(output_shape)
+
+	matrix = cv2.getPerspectiveTransform(src, dst)
+
+	return PerspectiveTransform(matrix, output_shape)
